88dd09d0ad7c98cf575267780832165bd4a189be
Siket Mate <msiket@iit.edu>
2025-04-23 13:03:18 +0200

    jit disabled in cli

--- interface_cli.py
+++ interface_cli.py
@@ -1,6 +1,10 @@
+from numba import jit, config
+config.DISABLE_JIT = True
+
 from pymgipsim.main import run_simulator_cli
 from pymgipsim.Interface.parser import generate_parser_cli
 import matplotlib.pyplot as plt
+
 
 """ Parse Arguments  """
 args = generate_parser_cli().parse_args()
